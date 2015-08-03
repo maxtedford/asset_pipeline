2d916f68166e271cacc12e5b38cc1a5649acd20a
Max Tedford <max.tedford@gmail.com>
2015-08-03 09:50:33 -0600

    add additional path and load it up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join("app", "flash", "assets")
+Rails.application.config.assets.paths << Rails.root.join("app", "pitted", "assets")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
